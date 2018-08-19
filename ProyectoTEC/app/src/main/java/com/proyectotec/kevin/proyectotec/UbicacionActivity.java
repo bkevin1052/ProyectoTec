bd4b9f1dc6a8a6d11cd1c87872312b0a69253c7b
Kevin Estuardo Barrientos Calderon <bkevin1052@gmail.com>
2018-08-18 23:21:39 -0600

    Agregando nuevas funcionalidades

--- ProyectoTEC/app/src/main/java/com/proyectotec/kevin/proyectotec/UbicacionActivity.java
+++ ProyectoTEC/app/src/main/java/com/proyectotec/kevin/proyectotec/UbicacionActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -27,5 +30,31 @@ public class UbicacionActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id==R.id.menu_acerca){
+
+        }
+        if(id==R.id.menu_configuracion){
+
+        }
+        if(id==R.id.menu_contacto){
+
+        }
+        if(id==R.id.menu_compartir){
+            startActivity(new Intent(UbicacionActivity.this,EnviarListaActivity.class));
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
