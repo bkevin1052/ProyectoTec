52359997afc9b71e432c0893f3ea26a6e5b260b4
Kevin Estuardo Barrientos Calderon <bkevin1052@gmail.com>
2018-08-23 21:46:40 -0600

    Cambiando background

--- ProyectoTEC/app/src/main/java/com/proyectotec/kevin/proyectotec/SplashScreenActivity.java
+++ ProyectoTEC/app/src/main/java/com/proyectotec/kevin/proyectotec/SplashScreenActivity.java
@@ -1,22 +1,34 @@
 package com.proyectotec.kevin.proyectotec;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends Activity {
 
+    ImageView imagen;
+    TextView superior,inferior;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashScreenActivity.this,PrincipalActivity.class));
-            }
-        },2000);
+        imagen = (ImageView)findViewById(R.id.logoImagen);
+        superior = (TextView)findViewById(R.id.tituloSuperior);
+        inferior = (TextView)findViewById(R.id.tituloInferior);
+
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.up_to_down);
+
+        imagen.setAnimation(animation);
+        superior.setAnimation(animation);
+        inferior.setAnimation(animation);
+
+        new Handler().postDelayed(() -> startActivity(new Intent(SplashScreenActivity.this,PrincipalActivity.class)),5300);
     }
 }
